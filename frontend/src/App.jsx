d7ffe8e841d752238c8a4bd393e5cc7f421db40d
csmeby <connorsmeby@tamu.edu>
2025-11-20 14:44:00 -0600

    fix to cashier and app reflecting updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { SignedIn, SignedOut, UserButton, useUser, useSignIn, useSignUp } from '@clerk/clerk-react';
-import BobaKiosk from './BobaKiosk';
-import BobaManager from './BobaManager';
-import BobaCashier from './BobaCashier';
-import MenuBoardApp from './menu-board/MenuBoardApp';
+import BobaKiosk from './boba_kiosk';
+import BobaManager from './boba_manager';
+import BobaCashier from './boba_cashier';
+import MenuBoardApp from './menu_board/menu_board_app';
 
 // Custom Sign In Component
 function CustomSignIn({ onSuccess, onSwitchToSignUp }) {
