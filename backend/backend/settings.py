d590e6ac539d7edf628bfffd185d5ffe2fa76f7d
OwenW24 <owenweis@tamu.edu>
2025-11-24 16:32:54 -0600

    backend configured for google cloud

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,8 +16,20 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['project3-gang-20.onrender.com', '127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = []
 
+# This K_SERVICE_URL variable is set automatically by Cloud Run
+CLOUDRUN_SERVICE_URL = os.environ.get('K_SERVICE_URL')
+
+if CLOUDRUN_SERVICE_URL:
+    # This automatically gets your URL: 'project3-gang-20-810838872032.us-south1.run.app'
+    ALLOWED_HOSTS.append(CLOUDRUN_SERVICE_URL.split("://")[1])
+else:
+    # Fallback for local development
+    ALLOWED_HOSTS.append('127.0.0.1')
+    ALLOWED_HOSTS.append('localhost')
+
+ALLOWED_HOSTS.append('project3-gang-20.onrender.com')
 
 # Application definition
 
