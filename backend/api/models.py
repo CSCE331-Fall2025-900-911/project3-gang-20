42aa303f42dcd9ee4a95be50dc477903671ee978
OwenW24 <owenweis@tamu.edu>
2025-12-04 16:23:21 -0600

    added subtotal to orders

--- backend/api/models.py
+++ backend/api/models.py
@@ -160,7 +160,8 @@ class Order(models.Model):
     """
     order_date_time = models.DateTimeField(auto_now_add=True)
     payment_type = models.CharField(max_length=20) # e.g., "Cash", "Card"
-    
+    sub_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
     customer = models.ForeignKey(
         Customer, 
         on_delete=models.SET_NULL, # Keep order history if customer is deleted
